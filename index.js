18251d5613952472c69cafac27145e947218a8fd
g2love <shuy_ang@163.com>
2017-06-14 15:03:07 +0800

    Create index.js

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
-[master 6b952f6] index.ios.js=
- 2 files changed, 1 insertion(+), 1 deletion(-)
- rename index.ios.js => index.js (100%)
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Page from './page';
+
+class PageIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      PageAllNum:9,//总页数
+      activePage:8,//当前活动页码数
+      current:8,//回调的当前页面数
+    };
+  }
+  callBack(CB){
+    console.log(CB)
+    this.setState({
+      current:CB,
+      // activePage:CB,
+    })
+  }
+  render(){
+    return(
+      <View style={{flex:1}}>
+        <View style={styles.container}>
+          <Text>{this.state.current}</Text>
+        </View>
+        <Page PageAllNum={9} activePage={8} callBack={(CB)=>this.callBack(CB)}/>
+      </View>
+      
+    )
+  }
+}
+const styles = StyleSheet.create({
+  //父组件样式
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+
+
+AppRegistry.registerComponent('Page', () => PageIndex);
